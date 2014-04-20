8fac07be38d96a4f231cdae66e4fe9864d3e3112
colinpmacarthur <colinpmacarthur@gmail.com>
2014-04-20 15:50:41 -0700

    Update platforms.php

--- platforms.php
+++ platforms.php
@@ -53,9 +53,9 @@ $db->setQuarter(htmlspecialchars($_GET["quarter"]));
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        <li><a href="#">Popular Places</a></li>
+	        <li><a href="places.php?year=2013&quarter=3">Popular Places</a></li>
 	        <li class="active"><a href="#">Popular Platforms</a></li>
-	        <li><a href="#">Problems</a></li>
+	        <li><a href="sentimentanalysis.html">Negative Reviews</a></li>
 	      </ul>
 	      <ul class="nav navbar-nav navbar-right">
 	        <li class="dropdown">
