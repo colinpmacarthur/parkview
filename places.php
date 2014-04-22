d470b988a522da6495fee6c0f2fc82f4cdf6af1d
Colin MacArthur <colinpmacarthur@gmail.com>
2014-04-22 11:37:59 -0700

    adding map to places dashboard

--- places.php
+++ places.php
@@ -28,7 +28,32 @@ $db->setQuarter(htmlspecialchars($_GET["quarter"]));
     <!-- Loading Flat UI -->
     <link href="css/flat-ui.css" rel="stylesheet">
     <link rel="shortcut icon" href="images/favicon.ico">
- 
+ 	 <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="css/flat-ui.css" rel="stylesheet">
+		<link href="css/demo.css" rel="stylesheet">
+    <link rel="shortcut icon" href="parkview-master/images/favicon.ico">
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+		<script type="text/javascript" src="js/maplabel.js"></script>
+		<script src="http://people.ischool.berkeley.edu/~sasaki/fp/d3-master/d3.js"></script>
+    <script src="http://code.jquery.com/jquery.js"></script>
+		<script src="http://code.highcharts.com/highcharts.js"></script>
+		<script src="http://code.highcharts.com/modules/exporting.js"></script>
+    <!-- <script src="js/jquery-ui-1.10.3.custom.min.js"></script> -->
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+   <!-- <script src="parkview-master/js/jquery.stacktable.js"></script> -->
+    <script src="http://vjs.zencdn.net/4.3/video.js"></script>
+    <script src="js/application.js"></script>
+ <?php include 'chart_javascript_places.php'; ?>
+
+
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
@@ -86,9 +111,73 @@ $db->setQuarter(htmlspecialchars($_GET["quarter"]));
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-6">Test</div>
-				
-		</div>
+						<div class="col-md-6" id="map-canvas" style="height: 700px;">
+				<script>
+					var cityCircle;
+					var cityMarker;
+
+					function initialize() {
+
+					// Create the map.
+						var mapOptions = {
+							zoom: 12,
+							center: new google.maps.LatLng(40.497852, -121.420814),
+							mapTypeId: google.maps.MapTypeId.ROADMAP
+						}; //var mapOptions closed
+
+						var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+					//ordering php to get data
+						$.ajax({
+							type: 'POST',
+							url: 'getSnsRatingsByLocation.php',
+							data: {'year':<?php echo $db->getYear(); ?>, 'quarter':<?php echo $db->getQuarter(); ?>},
+							dataType: "json",
+
+							success: function(hoge) {
+								console.log(JSON.stringify(hoge));
+								//iterate making circles, markers, and infowindows for each point
+								//alert(hoge[0].ratingThree);
+								var i;
+								for (i =0; i<hoge.length; i++) {
+								// make a circle for i visiting site
+									var populationOptions = {
+										strokeColor: '#FF0000',
+										strokeOpacity: 0.8,
+										strokeWeight: 2,
+										fillColor: '#FF0000',
+										fillOpacity: 0.35,
+										map: map,
+										center: new google.maps.LatLng(hoge[i].latitude, hoge[i].longitude),
+										radius: hoge[i].totalMentions * (100),
+										clickable: true
+									}; //population option closed
+		 
+					 				// add the circle to the map.
+									cityCircle = new google.maps.Circle(populationOptions);
+
+									// create label
+									var mapLabel = new MapLabel({
+										text: hoge[i].siteName + ' : ' + hoge[i].totalMentions,
+										map: map,
+										fontSize: 12,
+										align: 'center'
+									});
+
+									// add the label to the map
+									mapLabel.set('position', new google.maps.LatLng(hoge[i].latitude, hoge[i].longitude));
+
+								} //for roop closed
+							}, //success closed
+							error: function(XMLHttpRequest, textStatus, errorThrown) {
+								alert('Error : ' + errorThrown);
+							} // error closed
+						}); //ajax closed
+					}; // function closed
+
+					google.maps.event.addDomListener(window, 'load', initialize);
+				</script>
+				</div>
 				<div class="col-md-6">
 					<b>Number of Mentions Over Time</b>
 					<div id="TotalActivityOverMonths" style="min-width: 310px; height: 150px; margin: 0 auto"></div>
@@ -136,24 +225,5 @@ $db->setQuarter(htmlspecialchars($_GET["quarter"]));
 
 
     <!-- Load JS here for greater good =============================-->
-    <script src="js/jquery-1.8.3.min.js"></script>
-    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
-    <script src="js/jquery.ui.touch-punch.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap-select.js"></script>
-    <script src="js/bootstrap-switch.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/flatui-checkbox.js"></script>
-    <script src="js/flatui-radio.js"></script>
-    <script src="js/jquery.tagsinput.js"></script>
-    <script src="js/jquery.placeholder.js"></script>
-  	<script src="http://code.highcharts.com/highcharts.js"></script>
-  	 <script src="js/application.js"></script>
-	<script src="http://code.highcharts.com/modules/exporting.js"></script>
-			<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-		<script type="text/javascript" src="js/maplabel.js"></script>
-		<script src="http://people.ischool.berkeley.edu/~sasaki/fp/d3-master/d3.js"></script>
-	<?php include 'chart_javascript_places.php'; ?>
-
   </body>
 </html>
